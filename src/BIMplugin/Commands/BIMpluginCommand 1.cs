1c408b2096ecbb5fa3432b9e995d6af1d69977be
Brian Johnson <Brian Johnson@W10-BJOHNSON>
2022-11-01 10:46:07 -0500

    Add project files.

---
+++ src/BIMplugin/Commands/BIMpluginCommand 1.cs
@@ -0,0 +1,61 @@
+﻿namespace BIMplugin
+{
+    using Autodesk.Revit.UI;
+    using Autodesk.Revit.DB;
+    using Dynamo.Applications;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Command code to be executed when button is clicked.
+    /// </summary>
+    /// <seealso cref="Autodesk.Revit.UI.IExternalApplication"/>
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+
+    public class BIMpluginCommand1 : IExternalCommand
+    {
+        #region public methods
+
+        /// <summary>
+        /// Executes the specified command data.
+        /// </summary>
+        /// <param name="commandData">The command data.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and documnet objects and start transaction
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            string Journal_Dynamo_Path = @"C:\BIMplugin\[ATU]SystemV4.dyn";
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData();
+            dynamoRevitCommandData.Application = commandData.Application;
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                { Dynamo.Applications.JournalKeys.ShowUiKey, false.ToString() }, // don't show DynamoUI at runtime
+                { Dynamo.Applications.JournalKeys.AutomationModeKey, true.ToString() }, //run journal automatically
+                { Dynamo.Applications.JournalKeys.DynPathKey, Journal_Dynamo_Path }, //run node at this file path
+                { Dynamo.Applications.JournalKeys.DynPathExecuteKey, true.ToString() }, // The journal file can specify if the Dynamo workspace opened from DynPathKey will be executed or not. If we are in automation mode the workspace will be executed regardless of this key.
+                { Dynamo.Applications.JournalKeys.ForceManualRunKey, false.ToString() }, // don't run in manual mode
+                { Dynamo.Applications.JournalKeys.ModelShutDownKey, true.ToString() },
+                { Dynamo.Applications.JournalKeys.ModelNodesInfo, false.ToString() }
+
+            };
+
+            dynamoRevitCommandData.JournalData = journalData;
+            Result externalCommandResult = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+            return externalCommandResult;
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ src/BIMplugin/Main.cs
@@ -0,0 +1,73 @@
+﻿namespace BIMplugin
+{
+    using System;
+    using System.Reflection;
+    using System.Windows.Media.Imaging;
+    using Autodesk.Revit.UI;
+    
+
+    /// <summary>
+    /// Plugin's main entry point.
+    /// </summary>
+    /// <seealso cref="Autodesk.Revit.UI.IExternalApplication"/>
+    public class Main : IExternalApplication
+    {
+        #region external application public methods
+        
+        /// <summary>
+        /// Called when [startup].
+        /// </summary>
+        /// <param name="application">The application.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            // Plugins main tab name.
+            string tabName = "Bim4Tips";
+
+            // Plugins name hoted on ribbon tab.
+            string panelAnnotationName = "Bim4Tips Programs";
+
+            // Create tab on Revit UI.
+            application.CreateRibbonTab(tabName);
+
+            // Create first panel on Revit ribbon tab.
+            var panelAnnotation = application.CreateRibbonPanel(tabName, panelAnnotationName);
+
+            // Create push button#1 data and populate it with information.
+            var BtnData1 = new PushButtonData("BtnData1", "Space \n Naming", Assembly.GetExecutingAssembly().Location, "BIMplugin.BIMpluginCommand")
+            {
+                ///ToolTipImage = new BitmapImage(new Uri(@"image location on disk..."));
+                ///ToolTip = "This is some sample tooltip text, replace it later...";
+            };
+            var Btn1 = panelAnnotation.AddItem(BtnData1) as PushButton;
+            Btn1.LargeImage = new BitmapImage(new Uri(@"C:\BIMplugin\res\LOGO_Temp_Image_32x32.png"));
+
+            // Create push button#2 data and populate it with information.
+            var BtnData2 = new PushButtonData("BtnData2", "[ATU]System", Assembly.GetExecutingAssembly().Location, "BIMplugin.BIMpluginCommand1")
+            {
+                ///ToolTipImage = new BitmapImage(new Uri(@"image location on disk..."));
+                ///ToolTip = "This is some sample tooltip text, replace it later...";
+            };
+            var Btn2 = panelAnnotation.AddItem(BtnData2) as PushButton;
+            Btn2.LargeImage = new BitmapImage(new Uri(@"C:\BIMplugin\res\LOGO_Image_32x32.png"));
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Called when [shutdown].
+        /// </summary>
+        /// <param name="application">The application.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        #endregion
+    }
+}
